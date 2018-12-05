441684003d0ac8e4802f963b1387df7bcb30dabe
unknown <jolaosoyusuf@gmail.com>
2018-12-05 22:34:06 +0100

    rspec started

--- spec/rave_ruby_spec.rb
+++ spec/rave_ruby_spec.rb
@@ -1,9 +1,21 @@
+
+require 'spec_helper'
+
 RSpec.describe RaveRuby do
+
+  test_public_key = "FLWSECK-61037cfe3cfc53b03e339ee201fa98f5-X"
+  test_secret_key = "FLWPUBK-92e93a5c487ad64939327052e113c813-X"
+
   it "has a version number" do
     expect(RaveRuby::VERSION).not_to be nil
   end
 
   it "does something useful" do
     expect(false).to eq(true)
+  end
+
+  it "it should return the valid object" do
+    rave = RaveRuby.new(test_public_key, test_secret_key)
+		expect(rave.nil?).to eq(false)
   end
 end
